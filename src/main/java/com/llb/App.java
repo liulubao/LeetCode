b26d2240ecd6634830b3361c2c356c5b1eec1f6b
liulubao <1014840707@qq.com>
2017-12-19 20:00:52 +0800

    创建项目

---
+++ src/main/java/com/llb/App.java
@@ -0,0 +1,13 @@
+package com.llb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
