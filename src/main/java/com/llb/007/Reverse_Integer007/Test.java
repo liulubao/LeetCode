4afbd75c37d5ab33b2904a9510c84210d8a6aa30
liulubao <1014840707@qq.com>
2017-12-22 23:28:21 +0800

    6、7的解决方法，但是都需要优化一下，时间消耗太大

---
+++ src/main/java/com/llb/007/Reverse_Integer007/Test.java
@@ -0,0 +1,15 @@
+package Reverse_Integer007;
+
+/**
+ * Created by LiuLubao on 2017/12/22.
+ */
+public class Test {
+    public static void main(String[] args){
+//        String s = "0123";
+//        int
+        System.out.println(Solution.reverse(1534236469));
+//        int i = 1534236469;
+//        System.out.println(Integer.reverse(1534236469));
+
+    }
+}
