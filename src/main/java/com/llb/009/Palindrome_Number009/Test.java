51ebabd2e4424508d7f76f61cf3ce3d4bc8d33e9
liulubao <1014840707@qq.com>
2017-12-23 16:06:41 +0800

    第九题解决

---
+++ src/main/java/com/llb/009/Palindrome_Number009/Test.java
@@ -0,0 +1,11 @@
+package Palindrome_Number009;
+
+/**
+ * Created by LiuLubao on 2017/12/23.
+ */
+public class Test {
+    public static void main(String[] args) {
+        int x=465;
+        System.out.println(Solution.isPalindrome(-3223));
+    }
+}
